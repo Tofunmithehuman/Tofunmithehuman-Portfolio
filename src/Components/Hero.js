a28f86473b150eee356990fef74c03b70a3cfaf2
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-12-31 10:55:49 +0100

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -7,26 +7,24 @@ function Hero() {
   useAos();
 
   return (
-    <div className="Hero">
-      <div className="container">
-        <div data-aos="fade-up" className="contain">
-          Hi, I am Bolaji Oluwatofunmi Ashraf.
-          <br />
-          I'm a Fullstack Javascript Developer (MERN)
-        </div>
-        <div data-aos="fade-up" className="buttons">
-          <Link
-              to="https://drive.google.com/file/d/1a1b8Ua4f9pTT-uBA3e9PHBPM7i-PXstD/view?usp=drive_link"
-              target="_blank">
-            <button>
-                Download CV
-            </button>
-          </Link>
-          <button>Latest Project</button>
-        </div>
+  <div className="Hero">
+    <div className="container">
+      <div data-aos="fade-up" className="contain">
+        Hi, I am Bolaji Oluwatofunmi Ashraf.
+        <br />
+        I'm a Fullstack Javascript Developer (MERN)
+      </div>
+      <div data-aos="fade-up" className="buttons">
+        <Link to="https://drive.google.com/file/d/1a1b8Ua4f9pTT-uBA3e9PHBPM7i-PXstD/view?usp=drive_link" target="_blank">
+          <button>
+            Download CV
+          </button>
+        </Link>
+        <button>Latest Project</button>
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default Hero;
