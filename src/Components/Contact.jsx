e22d534c55c464514c5dabe2f4423a2afb9d0df2
Bolaji Oluwatofunmi Ashraf <tofunmibolaji13@gmail.com>
2024-03-27 21:37:24 +0100

    conected contact to backend

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,18 +1,54 @@
 import React from "react";
 import "../Styles/Styles.css";
 import useAos from "./aos";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Contact() {
-  const handleClick = (event) => {
-    event.preventDefault();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
+  });
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch(
+        "https://tofunmithehuman-backend-production.up.railway.app/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const result = await response.text();
+      setSuccessMessage(result);
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, 5000);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   useAos();
   return (
     <div className="Contact" id="contact">
       <div className="container">
-        <form data-aos="fade-up">
+        <form data-aos="fade-up" onSubmit={handleSubmit}>
           <h3>
             Send a message <i className="fa-solid fa-envelope"></i>
           </h3>
@@ -20,34 +56,52 @@ function Contact() {
             name="name"
             type="text"
             placeholder="Full Name"
-            onClick={handleClick}
+            value={formData.name}
+            onChange={handleChange}
             required
           />
           <input
             name="email"
             type="email"
             placeholder="Email"
-            onClick={handleClick}
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            name="phone"
+            type="number"
+            placeholder="Phone Number"
+            value={formData.phone}
+            onChange={handleChange}
             required
           />
           <input
             name="subject"
             type="text"
             placeholder="Subject"
-            onClick={handleClick}
+            value={formData.subject}
+            onChange={handleChange}
             required
           />
           <textarea
             name="message"
             cols="30"
             rows="10"
-            placeholder="Message"
+            placeholder="Message "
+            value={formData.message}
+            onChange={handleChange}
             required
-            onClick={handleClick}
           ></textarea>
-          <Link to="#">
-            Submit <i className="fa-solid fa-paper-plane"></i>
-          </Link>
+          <button disabled={submitting}>
+            {submitting ? "Submitting" : "Submit"}{" "}
+            <i className="fa-solid fa-paper-plane"></i>
+          </button>
+          {successMessage && (
+            <p id="success">
+              {successMessage} <i className="fa-solid fa-check"></i>
+            </p>
+          )}
           <p>
             *To get more information about me and how i can help you work on
             projects, please fill out the form. I will get back to you as soon
